f4f12eded7a704cc6e5f0b6eec36014d3e55ebfb
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-04-22 18:03:04 +0600

    initial setup done

---
+++ src/components/shared/header/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import style from "../../style/navbar.module.css"
+
+function Navbar() {
+    const handleChoice = () => {
+
+    }
+    return (
+        <>
+            <div className={style.topNav}></div>
+            <div className={style.navbarContainter}>
+
+                <div className="container flex items-center justify-end space-x-10">
+                    <Link>Find Doctors</Link>
+                    <Link>Medicines</Link>
+                    <Link>Contact</Link>
+                    <Link>About us</Link>
+                    <Link>Facilites</Link>
+                    <Link className={style.signup}>Signup</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
