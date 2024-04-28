3c743490bb71970699a7ef0cea7972defdf0c686
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-04-28 22:00:04 +0600

    working on debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Promise from './Promise';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Promise/>
     </>
   )
 }
---
+++ src/Promise.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const PromiseComponent = () => {
+    const [searchText, setSearchText] = useState("");
+
+    const debounce = (func, delay) => {
+        let timeoutId;
+
+        return (...args) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    };
+
+
+
+    const handleInput = (event) => {
+        const value = event.target.value;
+        debouning(value)
+    };
+
+    const handleSearch = (data) => {
+        setSearchText(data);
+    };
+    const debouning = debounce(handleSearch, 500);
+    console.log(searchText);
+
+    return (
+        <div className="text-black">
+            <h1>Hello</h1>
+            <input onChange={(e) => handleInput(e)} placeholder="Write here" />
+            <p>{searchText}</p>
+        </div>
+    );
+};
+
+export default PromiseComponent;
