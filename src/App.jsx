4452e865da0dc12a3f360a10f8ed4d7d8710f02b
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-05-05 14:02:42 +0600

    working on bug fixed:image,filter data, login button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './App.css'
 import Home from './components/Home/Home';
-import { setDoctorLogInfo, setUserLogInfo } from './Redux/Features/authSlice';
-import { useNavigate } from 'react-router-dom';
+import { setDoctorLogInfo } from './Redux/Features/authSlice';
 import { useEffect } from 'react';
-import { setAppointmentInfo } from './Redux/Features/filterSlice';
+
 
 function App() {
   const dispatch = useDispatch();
